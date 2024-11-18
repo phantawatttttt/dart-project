0f36d834b1403181bbd573cc6199b34d469da68d
phantawatttttt <phantawatm65@nu.ac.th>
2024-11-18 16:50:08 +0700

    โจทย์ข้อ 7

---
+++ reminder.dart
@@ -0,0 +1,8 @@
+void main() {
+    int n1 = 10;
+    int n2 = 3;
+    int quotuebt = (n ~/  n2);
+    int remainder = n1 % n2;
+    print('quotient = $quotient');
+    print('remainder = $remainder');
+}
